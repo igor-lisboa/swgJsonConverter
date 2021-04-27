801f6884e699d53b672e5a9d03767303b3fbe238
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-27 12:45:50 -0300

    Iniciando tela q vai realizar a conversao

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="d-flex h-100 text-center text-white bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto row">
+        <h3 class="float-md-start mb-0 mx-2 text-white">SWG JSON CONVERTER</h3>
+      </header>
+
+      <main class="px-3">
+        <converter />
+      </main>
+
+      <footer class="mt-auto text-white-50">
+        <p>
+          Developed by
+          <a
+            href="https://github.com/igor-lisboa"
+            target="_blank"
+            class="text-white"
+            >LISBÔA, Igor</a
+          >.
+        </p>
+      </footer>
+    </div>
+  </div>
 </template>
-
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineComponent } from "vue";
+import Converter from "./components/Converter.vue";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  components: { Converter },
+  name: "App",
+});
 </script>
-
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  display: grid;
 }
 </style>
---
+++ src/components/Converter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Put your JSON here</span>
+      </div>
+      <textarea class="form-control" v-model="json"></textarea>
+      <div class="input-group-append">
+        <button class="btn btn-warning" type="button" @click="convert">Convert</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "Converter",
+  setup: () => {
+    const json = ref("{}");
+    const convert = () => {
+      alert(json.value);
+    };
+    return { json, convert };
+  },
+});
+</script>
